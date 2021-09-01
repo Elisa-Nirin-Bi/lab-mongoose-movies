032f94bed6a8d626805157d51b61f69e5c722497
Elisa Barontini <nirinproductions@gmail.com>
2021-09-01 19:35:54 +0100

    movies routers needs to be impemented

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -51,4 +51,41 @@ celebritiesRouter.post('/celebrities', (req, res, next) => {
     });
 });
 
+celebritiesRouter.post('/celebrities/:id/delete', (request, response, next) => {
+  const id = request.params.id;
+  Celebrity.findByIdAndRemove(id)
+    .then(() => {
+      response.redirect('/celebrities');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebritiesRouter.get('/celebrities/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      res.render('celebrities/edit', { celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+celebritiesRouter.post('/celebrities/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  const name = req.params.name;
+  const occupation = req.params.occupation;
+  const catchPhrase = req.params.catchPhrase;
+
+  Celebrity.findByIdAndUpdate(id, { name, occupation, catchPhrase })
+    .then(() => {
+      res.redirect(`/celebrities/${id}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
 module.exports = celebritiesRouter;
